8f6cd95b4ecff14757c80a5e6b1092909ac42506
Jerry Velazquez <jvr572@gmail.com>
2019-09-17 17:10:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+let min = array[0],
+minN = 0;
+for (let i = 0; array.length > i ; i++)
+if (array[i] < min) {
+  min = array[i]
+  minN = i
+}
+array.splice(minN, 1)
+return min
 }
 
 function insertionSort(array){
-
+  let min,
+  sorted = []
+  while(array.length !== 0) {
+    min = findMinAndRemove(array)
+    sorted.push(min)
+  }
+  return sorted
 }
